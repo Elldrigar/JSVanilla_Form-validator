f8bb2984c5c50a4283ec4d5fce7a28ffc1e7460c
Artur Gawron <artur@gawron.me>
2020-02-08 23:08:21 +0100

    Fix email validation using regular expressions

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
+//CHECK EMAIL VALID
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
@@ -28,6 +34,8 @@ form.addEventListener('submit', function (e) {
     }
     if (email.value === '') {
         showError(email, 'Email is required!');
+    } else if (!isValidEmail(email.value)) {
+        showError(email, 'Enter correct email!')
     } else {
         showSuccess(email);
     }
