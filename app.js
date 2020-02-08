ec9a77903ba78087ba62397ce81027bdd1951471
Artur Gawron <artur@gawron.me>
2020-02-08 22:41:31 +0100

    Add username validation

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+
+};
+
 form.addEventListener('submit', function (e) {
     e.preventDefault();
+
+    if (username.value === '') {
+        showError(username, 'Username is required!');
+    } else {
+        showSuccess(username);
+    }
 
     console.log('submit');
 });
